a879cad83b6d10db7e4b9fe1e448c8ea93dd9c8f
X.Wei <wistar.sjtu@gmail.com>
2012-04-10 21:34:01 +0800

    1st version of the blog

---
+++ posts/soft/chrome_background.md
@@ -0,0 +1,24 @@
+Title: chrome护眼设置--把背景设置为绿豆沙
+Slug: chrome-background
+Tags: tips, google,外观
+Date: 2012-02-23
+
+
+直接把[这里](http://hi.baidu.com/laolao18k/blog/item/3a268016cd4c4907c83d6d46.html)的贴上吧...
+(以下为copy)
+
+ 首先，下载安装chrome的[stylist插件](https://chrome.google.com/extensions/detail/pabfempgigicdjjlccdgnbmeggkbjdhd)
+然后，打开"扩展设置"，点击chrome stylist的选项，点击demo进行修改。 
+
+把网页背景修改为豆沙绿的参数设置: 
+输入框1：demo 选项框2：regexp 输入框3：(ftp|http|https)://\D 输入框4： * { background: #C7EDCC !important; } 修改后保存即可 (url和style text可根据自己喜好配置) 
+附：豆沙绿的参数 RGB颜色 199；237；204 十六位颜色代码 #C7EDCC 色调：85；饱和度：123；亮度：205 
+
+不会的话直接用[这个扩展](https://chrome.google.com/extensions/detail/hnjebfhieiaohnhafcolehbbcfkkkhje#%E5%AE%83%E9%99%A4%E4%BA%86%E6%8A%8AGoogle%E7%9A%84%E8%83%8C%E6%99%AF%E6%94%B9%E4%B8%BA%E4%BF%9D%E6%8A%A4%E7%9C%BC%E7%9D%9B%E7%9A%84%E7%BB%BF%E8%89%B2%EF%BC%8C%E4%BB%96%E6%B2%A1%E6%9C%89%E5%88%AB%E7%9A%84%E5%8A%9F%E8%83%BD)。
+![](./chrome-background/pasted_image.png)
+
+
+
+**--imported from zim**
+
+
---
+++ settings.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import sys
+
+TIMEZONE = 'Asia/Shanghai'
+
+DATE_FORMATS = {
+    'en' : u'%a, %d %b %Y',
+    'zh' : u'%Y-%m-%d',
+    'zhs': u'%Y-%m-%d',
+}
+# windows locale: http://msdn.microsoft.com/en-us/library/cdax410z%28VS.71%29.aspx
+#~ LOCALE = ['usa', 'cht', 'chs', 'jpn',        # windows
+          #~ 'en_US', 'zh_CN', 'ja_JP']  # Unix/Linux
+DEFAULT_LANG = 'zhs'
+
+SITENAME = "X. Wei's Blog"
+AUTHOR = 'X.Wei'
+
+DISQUS_SITENAME = 'farseerfcgithub'
+GITHUB_URL = 'https://github.com/X-Wei'
+SITEURL = 'http://x-wei.github.com'
+#~ GOOGLE_ANALYTICS = 'UA-29540705-1'
+TAG_FEED  = 'feeds/%s.atom.xml'
+
+
+SOCIAL = (
+          ('github', 'https://github.com/x-wei'),
+          )
+          #('twitter', 'http://twitter.com/farseerfc'),
+          #~ ('facebook', 'http://www.facebook.com/farseerfc'),
+          #~ ('weibo', 'http://weibo.com/farseerfc'),
+          #~ ('renren', 'http://www.renren.com/farseer'),
+          
+          
+
+#~ TWITTER_USERNAME = 'farseerfc'
+#~ SIDEBAR_CUSTOM = r"""
+#~ <li class="nav-header"><h4><i class="icon-list-alt"></i>Weibo</h4></li>
+#~ <iframe width="100%" height="550" class="share_self"  frameborder="0" scrolling="no" 
+#~ src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=1&ptype=1&speed=0&skin=2&isTitle=1&noborder=1&isWeibo=1&isFans=1&uid=1862842353&verifier=b193b9de&dpc=1">
+#~ </iframe>
+#~ """
+
+#~ GOOGLE_CUSTOM_SEARCH_SIDEBAR = "001578481551708017171:axpo6yvtdyg"
+#~ GOOGLE_CUSTOM_SEARCH_NAVBAR = "001578481551708017171:hxkva69brmg"
+
+#THEME='bootstrap2'
+THEME='notmyidea'
+#CSS_FILE = "wide.css"
+
+DEFAULT_CATEGORY ='MISC'
+OUTPUT_PATH = '.'
+PATH = 'posts'
+
+#PDF_GENERATOR = True
+
